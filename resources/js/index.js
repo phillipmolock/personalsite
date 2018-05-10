00cc9ba2ff48d583a0392ba7356e24e95262c485
phillipmolock <phillip.molock@outlook.com>
2018-05-10 16:40:21 -0500

    completed first run of marked up resume content and navigation

---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+// Jquery for Nav
+
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
